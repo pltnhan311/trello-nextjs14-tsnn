893ba23b105c029f6bffc3fb77075129ca0399db
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2024-05-23 22:34:46 +0700

    update ui components

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,12 +1,57 @@
 'use client';
 
+import { FormInput } from '@/components/form/form-input';
 import { Button } from '@/components/ui/button';
 import { Board } from '@prisma/client';
+import { ElementRef, useEffect, useRef, useState } from 'react';
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
-  return <Button variant={'transparent'} className='font-bold text-lg h-auto w-auto p-1 px-2'>{data.title}</Button>;
+  const formRef = useRef<ElementRef<'form'>>(null);
+  const inputRef = useRef<ElementRef<'input'>>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableEditing = () => {
+    console.log('alo');
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    console.log('I am submiting', title);
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
+  if (isEditing) {
+    <form ref={formRef} action={onSubmit} className='flex items-center gap-x-2'>
+      <FormInput
+        ref={inputRef}
+        id='title'
+        onBlur={onBlur}
+        defaultValue={data.title}
+        className='text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent bg-none'
+      />
+    </form>;
+  }
+  console.log(isEditing);
+  return (
+    <Button variant='transparent' className='font-bold text-lg h-auto w-auto p-1 px-2' onClick={enableEditing}>
+      {data.title}
+    </Button>
+  );
 };
--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -2,9 +2,11 @@
 
 import { forwardRef } from 'react';
 import { useFormStatus } from 'react-dom';
+
+import { cn } from '@/lib/utils';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { cn } from '@/lib/utils';
+
 import { FormErrors } from './form-errors';
 
 interface FormInputProps {
@@ -21,14 +23,14 @@ interface FormInputProps {
 }
 
 export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
-  ({ id, label, type = 'text', placeholder, required, disabled, errors, className, defaultValue, onBlur }, ref) => {
+  ({ id, label, type, placeholder, required, disabled, errors, className, defaultValue = '', onBlur }, ref) => {
     const { pending } = useFormStatus();
 
     return (
       <div className='space-y-2'>
         <div className='space-y-1'>
           {label ? (
-            <Label htmlFor={id} className='text-sm font-semibold text-neutral-700'>
+            <Label htmlFor={id} className='text-xs font-semibold text-neutral-700'>
               {label}
             </Label>
           ) : null}
@@ -40,6 +42,8 @@ export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
             name={id}
             id={id}
             placeholder={placeholder}
+            type={type}
+            disabled={pending || disabled}
             className={cn('text-sm px-2 py-1 h-7', className)}
             aria-describedby={`${id}-error`}
           />
