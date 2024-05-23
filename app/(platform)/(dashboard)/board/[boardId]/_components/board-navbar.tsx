35cfe738b9b64dfaf5a51b9e982c272fac81aa8f
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2024-05-23 23:09:06 +0700

    update finish CRUD board page

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,3 +1,4 @@
+import { BoardOptions } from '@/app/(platform)/(dashboard)/board/[boardId]/_components/board-options';
 import { BoardTitleForm } from '@/app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form';
 import { Board } from '@prisma/client';
 
@@ -9,6 +10,9 @@ export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
   return (
     <div className='w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white'>
       <BoardTitleForm data={data} />
+      <div className='ml-auto'>
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,22 +1,36 @@
 'use client';
 
+import { updateBoard } from '@/actions/update-board';
 import { FormInput } from '@/components/form/form-input';
 import { Button } from '@/components/ui/button';
+import { useAction } from '@/hooks/use-action';
 import { Board } from '@prisma/client';
 import { ElementRef, useEffect, useRef, useState } from 'react';
+import { toast } from 'sonner';
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board ${title} updated!`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    }
+  });
+
   const formRef = useRef<ElementRef<'form'>>(null);
   const inputRef = useRef<ElementRef<'input'>>(null);
 
+  const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
   const enableEditing = () => {
-    console.log('alo');
     setIsEditing(true);
     setTimeout(() => {
       inputRef.current?.focus();
@@ -30,7 +44,10 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string;
-    console.log('I am submiting', title);
+    execute({
+      title,
+      id: data.id
+    });
   };
 
   const onBlur = () => {
@@ -38,20 +55,22 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
   };
 
   if (isEditing) {
-    <form ref={formRef} action={onSubmit} className='flex items-center gap-x-2'>
-      <FormInput
-        ref={inputRef}
-        id='title'
-        onBlur={onBlur}
-        defaultValue={data.title}
-        className='text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent bg-none'
-      />
-    </form>;
+    return (
+      <form ref={formRef} action={onSubmit} className='flex items-center gap-x-2'>
+        <FormInput
+          ref={inputRef}
+          id='title'
+          onBlur={onBlur}
+          defaultValue={title}
+          className='text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent bg-none'
+        />
+      </form>
+    );
   }
-  console.log(isEditing);
+
   return (
     <Button variant='transparent' className='font-bold text-lg h-auto w-auto p-1 px-2' onClick={enableEditing}>
-      {data.title}
+      {title}
     </Button>
   );
 };
